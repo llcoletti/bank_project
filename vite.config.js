e475fa7ed50f20ab42dd423936c56a56a58e5cd3
llcoletti <llcoletti@universo.univates.br>
2023-05-15 11:35:09 -0300

    config vite ghpages

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite';
 import postcss from './postcss.config.js';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-
-//   plugins: [react()],
-//   css: {
-//     postcss,
-//   },
-// });
-
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
