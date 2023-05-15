90be715c76f253502cd83b6f24a96fa88a5b9d03
llcoletti <llcoletti@universo.univates.br>
2023-05-15 11:31:59 -0300

    configuracao vite ghpages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,26 @@ import postcss from './postcss.config.js';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss,
-  },
+// export default defineConfig({
+
+//   plugins: [react()],
+//   css: {
+//     postcss,
+//   },
+// });
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+    css: {
+      postcss,
+    },
+  };
+
+  if (command !== 'serve') {
+    config.base = '/bank_project/';
+  }
+
+  return config;
 });
